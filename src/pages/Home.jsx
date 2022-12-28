497b8e2b2cbde6067df81fee89d349a914487c16
VDias88 <viniciusmonteirodias5@gmail.com>
2022-12-28 17:07:03 -0300

    Concertando bugs resolvendo background sound

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import {
   ListItem,
   Text,
 } from 'react-native-ui-lib'
-import { Audio } from 'expo-av'
+import { Audio, InterruptionModeIOS, InterruptionModeAndroid } from 'expo-av'
 import { observer } from 'mobx-react'
 import Container from '../components/Container'
 import MusicsStore from '../stores/musics/MusicsStore'
@@ -14,25 +14,45 @@ import MusicRow from './components/MusicRow'
 
 const store = new MusicsStore()
 const Home = observer(({ navigation }) => {
-  const [sound, setSound] = useState()
+  const [currentSound, setSound] = useState()
+  const [currenStatus, seStatus] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await Audio.setAudioModeAsync({
+        staysActiveInBackground: true,
+        interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+        shouldDuckAndroid: true,
+        playThroughEarpieceAndroid: true,
+        allowsRecordingIOS: true,
+        interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+        playsInSilentModeIOS: true,
+      })
+    }
+    fetchData()
+  }, [])
   useEffect(() => {
     store.getMusics()
   }, [])
+  useEffect(() => {
+    console.log(currenStatus)
+  }, [currenStatus])
   async function playMusic(music) {
-    const { sound } = await Audio.Sound.createAsync({ uri: music }, { shouldPlay: true })
+    const { sound, status } = await Audio.Sound.createAsync({ uri: music }, { shouldPlay: true, staysActiveInBackground: true })
     setSound(sound)
-    await sound.playAsync()
+    seStatus(status)
+    await currentSound.playAsync()
   }
   function stopMusic() {
-    sound.unloadAsync()
+    currentSound.unloadAsync()
   }
   return (
     <Container>
-      {/* {store.isFetching && <LoaderScreen color="white" />}
+      {store.isFetching && <LoaderScreen color="white" />}
       <Button onPress={() => stopMusic()}><Text>Parar</Text></Button>
       {store.musics?.map((e, i) => (
         <MusicRow title={e.filename} time={e.duration} key={i} onPress={() => playMusic(e.uri)} />
-      ))} */}
+      ))}
     </Container>
   )
 })
